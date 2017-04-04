0d426b63a8f60d48b705b98371c72065999a11cc
Sadat Islam <sadat21@hotmail.ca>
2017-04-03 20:12:26 -0600

    Intialized Server and added Server fields

--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by Sadat Msi on 4/1/2017.
  */
 public class Server {
+
+    ServerSocket serverSocket;
+    Socket aSocket;
+    PrintWriter out;
+    BufferedReader in;
+
+    /**
+     * Intialize Server
+     * @throws IOException
+     */
+    public Server() throws IOException {
+        serverSocket = new ServerSocket(10);
+        System.out.println("Server is running...");
+        in = new BufferedReader(new InputStreamReader(aSocket.getInputStream()));
+        out = new PrintWriter((aSocket.getOutputStream()), true);
+    }
+
+
 
 
 
@@ -10,6 +35,14 @@ public class Server {
     public static void main(String [] args){
         //Intialize Database
         DataBase db = new DataBase();
+        //Intialize Server
+        Server s;
+        try {
+            s = new Server();
+        } catch (IOException e) {
+            System.err.println("Error turning on server");
+            e.printStackTrace();
+        }
 
 
     }
