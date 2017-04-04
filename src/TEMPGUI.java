3072312465158a0b59fb228c2eb1dc8e0a958bb9
Brian Pho <brianpho7@gmail.com>
2017-04-04 11:42:54 -0600

    Created TEMPGUI

---
+++ src/TEMPGUI.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Brian on 2017-04-04.
+ */
+public class TEMPGUI extends PassengerGUI {
+    public static void main(String[] args)
+    {
+        TEMPGUI test = new TEMPGUI();
+        test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        test.setVisible(true);    }
+
+    public TEMPGUI()
+    {
+        super();
+    }
+}
