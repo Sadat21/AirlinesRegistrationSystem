abc850f0305f4fe6260170dfb0dd523a3eb10c97
Sadat Islam <sadat21@hotmail.ca>
2017-04-04 12:15:10 -0600

    Stashing changes

--- src/ConnectionConstants.java
+++ src/ConnectionConstants.java
@@ -3,6 +3,6 @@
  */
 public interface ConnectionConstants {
 
-    public static final String HOST = "localhost";
+    public static final String HOST = "192.168.1.75";
     public static final int PORT = 10;
 }
