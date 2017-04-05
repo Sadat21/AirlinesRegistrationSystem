f83c9925875793da3087f0ef625d52ad82b6d1fc
Brian Pho <brianpho7@gmail.com>
2017-04-04 23:23:32 -0600

    Created Admin.java

--- src/Admin.java
+++ src/Admin.java
@@ -3,5 +3,20 @@
  * @version 1.0
  * @since 4/1/2017
  */
-public class Admin extends Client {
+public class Admin extends Client
+{
+	AdminGUI myGUI;
+
+	public Admin(AdminGUI x){
+		super();
+		myGUI = x;
+		myGUI.main(null);
+	}
+
+
+	public static void main(String [] args){
+		AdminGUI myGUI = new AdminGUI();
+		Admin me = new Admin(myGUI);
+		me.communicate();
+	}
 }
