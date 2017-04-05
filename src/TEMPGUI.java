4e2b5e5b316f55f6bf8ab132ed4b59ea932723b1
Brian Pho <brianpho7@gmail.com>
2017-04-04 18:20:36 -0600

    Starting the changes to made AdminGUI extended

--- src/TEMPGUI.java
+++ src/TEMPGUI.java
@@ -7,6 +7,11 @@ import java.awt.*;
 public class TEMPGUI extends PassengerGUI {
     public static void main(String[] args)
     {
+        try {
+            UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         TEMPGUI test = new TEMPGUI();
         test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         test.setVisible(true);
@@ -15,5 +20,7 @@ public class TEMPGUI extends PassengerGUI {
     public TEMPGUI()
     {
         super();
+        setTitle("Admin Client Program");
+        setSize(1295, 680);
     }
 }
