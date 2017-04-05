d2f7a94e4b543ebec7a09a8952632accf2f895ff
Sadat Islam <sadat21@hotmail.ca>
2017-04-05 14:48:54 -0600

    Changed IP

--- src/ConnectionConstants.java
+++ src/ConnectionConstants.java
@@ -3,7 +3,7 @@
  */
 public interface ConnectionConstants {
 
-    public static final String HOST = "192.168.1.75";
+    public static final String HOST = "10.13.126.70";
     public static final int PORT = 10;
     public static final String DEFAULT = "-1\n";
 }
