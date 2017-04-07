e6012caf3695d594cff01ec842fe10b54c8854b8
Brian Pho <brianpho7@gmail.com>
2017-04-07 00:02:54 -0600

    Created Login GUI

---
+++ src/LoginGUITEMP.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+/**
+ * @author brain
+ * @version 1.0
+ * @since 4/6/2017
+ */
+public class LoginGUITEMP {
+	private JPanel MainPanel;
+	private JButton Sign_In;
+	private JLabel TitleLabel;
+	private JTextField textField1;
+	private JPasswordField passwordField1;
+	private JPanel PanelTwo;
+	private JPanel PasswordPanel;
+	private JPanel SignInButtonPanel;
+	private JPanel TitlePanel;
+	private JPanel PanelOne;
+	private JPanel SignUpButtonPanel;
+}
