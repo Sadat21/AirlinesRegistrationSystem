bbe8f7f7f03fa18327348718fb23f2ca38e6a50a
Sadat Islam <sadat21@hotmail.ca>
2017-04-07 01:55:48 -0600

    Harjee's adminGUI

--- src/AdminGUI.java
+++ src/AdminGUI.java
@@ -49,14 +49,49 @@ public class AdminGUI extends PassengerGUI implements ListSelectionListener
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() == addFlightButton)
             {
-                //JFrame temp = new JFrame();
-                AddFlightPanel temp = new AddFlightPanel();
-                //temp.add(getPanelThree());
-                //temp.setSize(270, 470);
-                temp.setVisible(true);
+                System.out.println("Yeet");
+
+
+
+
+
+
+
+
+
             }
+
+            else if(e.getSource() == clearButton)
+            {
+                TFL1.setText("");
+                TFL2.setText("");
+                TFL3.setText("");
+                TFL4.setText("");
+                monthCB.setSelectedItem("-");
+                dayCB.setSelectedItem("-");
+                yearCB.setSelectedItem("-");
+                monthDDCB.setSelectedItem("-");
+                dayDDCB.setSelectedItem("-");
+                yearDDCB.setSelectedItem("-");
+                listModel.clear();
+                TFR1.setText("");
+                TFR2.setText("");
+                TFR3.setText("");
+                TFR4.setText("");
+                TFR5.setText("");
+                TFR6.setText("");
+                TFR7.setText("");
+                TFR8.setText("");
+                TFR9.setText("");
+                TFR10.setText("");
+                TFRR1.setText("");
+                TFRR2.setText("");
+                TFRR3.setText("");
+            }
+
             else if (e.getSource() == addFlightsFromFileButton)
             {
+
                 String fileName = JOptionPane.showInputDialog("Enter the file name: ");
 
                 if(!fileName.endsWith(".txt"))
@@ -169,6 +204,7 @@ public class AdminGUI extends PassengerGUI implements ListSelectionListener
                     JOptionPane.showMessageDialog(null, "Please enter the required information into the 'View Ticket' text fields");
                     return;
                 }
+
                 String temp = "SEARCHTICKET\t" + ID + "\t" + src + "\t" + dst;
                 System.out.println(temp);
                 Global.toGo = temp;
@@ -177,249 +213,11 @@ public class AdminGUI extends PassengerGUI implements ListSelectionListener
             else if (e.getSource() == cancelTicket) {
                 // I'm assuming that the ID is the first value in the ticket info
                 String temp = "CANCELTICKET\t";
-                if (searchResultsTickets.getSelectedIndex() != -1) {
-                    String info = searchResultsTickets.getSelectedValue();
-                    String[] data = info.split(" ");
-                    temp += data[1] + "\t";
-                    temp += data[3];
-                    Global.toGo = temp;
-                }
-                else
-                {
-                    JOptionPane.showMessageDialog(null, "Please select a ticket.");
-                }
-            }
-        }
-    }
-
-    private class AddFlightPanel extends JFrame
-    {
-        private JPanel FIPanel;
-        private JLabel FlightInfo;
-        private JSeparator Sep3;
-        private JLabel LabelR1, LabelR2, LabelR3, LabelR4, LabelR5, LabelR6, LabelR7, LabelR8, LabelR9, LabelR10;
-        private JTextField TFR1, TFR2, TFR3, TFR4, TFR5, TFR6, TFR7, TFR8, TFR9, TFR10;
-        private GridBagConstraints gbc;
-        private Container c;
-        private JButton addFlight, clear;
-        private innerlistener listen;
-
-        public AddFlightPanel()
-        {
-            setTitle("Add Flight Panel");
-            setSize(300, 520);
-            c = getContentPane();
-            listen = new innerlistener();
-            FIPanel = new JPanel();
-            FIPanel.setLayout(new GridBagLayout());
-            c.add(FIPanel);
-            FlightInfo = new JLabel();
-            FlightInfo.setFont(new Font(FlightInfo.getFont().getName(), Font.BOLD, 24));
-            FlightInfo.setText("Add Flight Information");
-            gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 0;
-            gbc.gridwidth = 3;
-            FIPanel.add(FlightInfo, gbc);
-            LabelR2 = new JLabel();
-            LabelR2.setText("From");
-            gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 3;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.insets = new Insets(10, 0, 10, 10);
-            FIPanel.add(LabelR2, gbc);
-            TFR2 = new JTextField();
-            TFR2.setColumns(10);
-            gbc = new GridBagConstraints();
-            gbc.gridx = 2;
-            gbc.gridy = 3;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            FIPanel.add(TFR2, gbc);
-            LabelR3 = new JLabel();
-            LabelR3.setText("To");
-            gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 4;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.insets = new Insets(10, 0, 10, 10);
-            FIPanel.add(LabelR3, gbc);
-            TFR3 = new JTextField();
-            TFR3.setColumns(10);
-            gbc = new GridBagConstraints();
-            gbc.gridx = 2;
-            gbc.gridy = 4;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            FIPanel.add(TFR3, gbc);
-            LabelR4 = new JLabel();
-            LabelR4.setText("Date");
-            gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 5;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.insets = new Insets(10, 0, 10, 10);
-            FIPanel.add(LabelR4, gbc);
-            LabelR5 = new JLabel();
-            LabelR5.setText("Time");
-            gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 6;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.insets = new Insets(10, 0, 10, 10);
-            FIPanel.add(LabelR5, gbc);
-            LabelR6 = new JLabel();
-            LabelR6.setText("Duration");
-            gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 7;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.insets = new Insets(10, 0, 10, 10);
-            FIPanel.add(LabelR6, gbc);
-            LabelR7 = new JLabel();
-            LabelR7.setText("Total Seats");
-            gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 8;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.insets = new Insets(10, 0, 10, 10);
-            FIPanel.add(LabelR7, gbc);
-            LabelR8 = new JLabel();
-            LabelR8.setText("Remaining Seats");
-            gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 9;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.insets = new Insets(10, 0, 10, 10);
-            FIPanel.add(LabelR8, gbc);
-            LabelR9 = new JLabel();
-            LabelR9.setText("Price");
-            gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 10;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.insets = new Insets(10, 0, 10, 10);
-            FIPanel.add(LabelR9, gbc);
-            TFR4 = new JTextField();
-            TFR4.setColumns(10);
-            gbc = new GridBagConstraints();
-            gbc.gridx = 2;
-            gbc.gridy = 5;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            FIPanel.add(TFR4, gbc);
-            TFR5 = new JTextField();
-            TFR5.setColumns(10);
-            gbc = new GridBagConstraints();
-            gbc.gridx = 2;
-            gbc.gridy = 6;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            FIPanel.add(TFR5, gbc);
-            TFR6 = new JTextField();
-            TFR6.setColumns(10);
-            gbc = new GridBagConstraints();
-            gbc.gridx = 2;
-            gbc.gridy = 7;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            FIPanel.add(TFR6, gbc);
-            TFR7 = new JTextField();
-            TFR7.setColumns(10);
-            gbc = new GridBagConstraints();
-            gbc.gridx = 2;
-            gbc.gridy = 8;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            FIPanel.add(TFR7, gbc);
-            TFR8 = new JTextField();
-            TFR8.setColumns(10);
-            gbc = new GridBagConstraints();
-            gbc.gridx = 2;
-            gbc.gridy = 9;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            FIPanel.add(TFR8, gbc);
-            TFR9 = new JTextField();
-            TFR9.setColumns(10);
-            gbc = new GridBagConstraints();
-            gbc.gridx = 2;
-            gbc.gridy = 10;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            FIPanel.add(TFR9, gbc);
-            Sep3 = new JSeparator();
-            gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 1;
-            gbc.gridwidth = 3;
-            gbc.fill = GridBagConstraints.BOTH;
-            gbc.insets = new Insets(10, 0, 10, 0);
-            FIPanel.add(Sep3, gbc);
-            TFR1 = new JTextField();
-            TFR1.setColumns(10);
-            gbc = new GridBagConstraints();
-            gbc.gridx = 2;
-            gbc.gridy = 2;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            FIPanel.add(TFR1, gbc);
-            LabelR1 = new JLabel();
-            LabelR1.setText("Flight Number");
-            gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 2;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.insets = new Insets(10, 0, 10, 10);
-            FIPanel.add(LabelR1, gbc);
-            LabelR10 = new JLabel();
-            LabelR10.setText("Price + Tax");
-            gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 11;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.insets = new Insets(10, 0, 10, 10);
-            FIPanel.add(LabelR10, gbc);
-            TFR10 = new JTextField();
-            TFR10.setColumns(10);
-            gbc = new GridBagConstraints();
-            gbc.gridx = 2;
-            gbc.gridy = 11;
-            gbc.anchor = GridBagConstraints.WEST;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            FIPanel.add(TFR10, gbc);
-            addFlight = new JButton("Add Flight");
-            gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 12;
-            gbc.anchor = GridBagConstraints.CENTER;
-            gbc.insets = new Insets(10, 0, 10, 10);
-            FIPanel.add(addFlight, gbc);
-            clear = new JButton("Clear");
-            gbc = new GridBagConstraints();
-            gbc.gridx = 2;
-            gbc.gridy = 12;
-            gbc.anchor = GridBagConstraints.CENTER;
-            gbc.insets = new Insets(10, 0, 10, 10);
-            FIPanel.add(clear, gbc);
-            addFlight.addActionListener(listen);
-            clear.addActionListener(listen);
-        }
-
-        class innerlistener implements ActionListener
-        {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                if (e.getSource() == clear)
-                {
-
-                }
-                else if (e.getSource() == addFlight)
-                {
-
-                }
+                String info = searchResultsTickets.getSelectedValue();
+                String[] data = info.split(" ");
+                temp += data[1] + "\t";
+                temp += data[3];
+                Global.toGo = temp;
             }
         }
     }
@@ -428,15 +226,12 @@ public class AdminGUI extends PassengerGUI implements ListSelectionListener
     {
         if (!e.getValueIsAdjusting())
         {
-            JList list = (JList) e.getSource();
             if (searchResultsTickets.getSelectedIndex() == -1)
             {
-            } else if (list.getName().equals("Flights")){
-                super.valueChanged(e);
-            } else if (list.getName().equals("Tickets"))
-            {
-                // Do nothing if they click on a ticket
-                System.out.println("Ticket " + String.valueOf(list.getSelectedIndex()));
+            } else {
+                int index = searchResultsTickets.getSelectedIndex();
+                System.out.println(searchResultsTickets.getSelectedIndex());
+                searchResultsTickets.setSelectedIndex(index);
             }
         }
     }
@@ -445,34 +240,21 @@ public class AdminGUI extends PassengerGUI implements ListSelectionListener
     {
         String[] temp = input.split("\t");
 
-        if (temp.length != 8) {
-            return true;
-        }
+        if (temp.length != 8) {return true;}
 
         for (int j = 0; j < temp.length; j++) {
-            if (temp[j].equals("")) {
-                return true;
-            }
+            if (temp[j].equals("")) {return true;}
         }
 
-        if ((temp[0].length() > 20) || (temp[1].length() > 20))
-        {
-            return true;
-        }
+        if ((temp[0].length() > 20) || (temp[1].length() > 20)) {return true;}
 
-        if (temp[2].length() > 10)
-        {
-            return true;
-        }
+        if (temp[2].length() > 10) {return true;}
 
-        if ((temp[3].length() > 8) || (temp[4].length() > 8))
-        {
-            return true;
-        }
+        if ((temp[3].length() > 8) || (temp[4].length() > 8)) {return true;}
 
         if(!temp[3].matches("([01]?[0-9]|2[0-3]):([0-5][0-9])")) {return true;}
 
-        if(!temp[4].matches("([0-9]{2}):([0-5][0-9]):([0-5][0-9])")) {return true;}
+        if(!temp[4].matches("([0-9]{2}):([01]?[0-9]|2[0-3]):([0-5][0-9])")) {return true;}
 
         try
         {
@@ -498,16 +280,11 @@ public class AdminGUI extends PassengerGUI implements ListSelectionListener
             depart = df.parse(temp[2]);
             if(!temp[2].equals(df.format(depart))) {return true;}
             Date curr = new Date();
-            if(curr.compareTo(depart) > 0)
-            {
-                return true;
-            }
+            if(curr.compareTo(depart) > 0) {return true;}
         }
-        catch(ParseException ex)
-        {
-            JOptionPane.showMessageDialog(null, "Error with date formatting");
-            return true;
-        }
+
+        catch(ParseException ex) {return true;}
+
         return false;
     }
 
@@ -672,18 +449,19 @@ public class AdminGUI extends PassengerGUI implements ListSelectionListener
         gbc.insets = new Insets(10, 0, 10, 0);
         MainPanel.add(TopSep, gbc);
         searchResultsTickets.addListSelectionListener(this);
+        clearButton.addActionListener(listener);
         addFlightButton.addActionListener(listener);
         addFlightsFromFileButton.addActionListener(listener);
         searchButton.addActionListener(listener);
         cancelTicket.addActionListener(listener);
-        searchResultsTickets.setName("Tickets");
+
     }
 
     private void setTickets(ArrayList<Ticket> tickets)
     {
-        listModel.clear();
         for (int i = 0; i < tickets.size(); i++)
         {
+            listModel.clear();
             listModel.addElement(tickets.get(i));
         }
     }
