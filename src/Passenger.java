c7dd41a8bb74fdcefae63c323a6426b5ad2115f9
Brian Pho <brianpho7@gmail.com>
2017-04-06 23:43:49 -0600

    Removed Print Statements

--- src/Passenger.java
+++ src/Passenger.java
@@ -10,24 +10,16 @@ public class Passenger extends Client {
     public Passenger(PassengerGUI x)
     {
         super();
-        System.out.println("Point 2.1");
         myGUI = x;
-        System.out.println("Point 2.2");
         myGUI.main(null);
-        System.out.println("Point 2.3");
     }
 
 
     public static void main(String [] args){
-        System.out.println("Point 1");
         PassengerGUI myGUI = new PassengerGUI();
-        System.out.println("Point 2");
         Passenger me = new Passenger(myGUI);
-        System.out.println("Point 3");
         me.setRef();
-        System.out.println("Point 4");
         me.communicate();
-        System.out.println("Point 5");
     }
 
     private void setRef()
